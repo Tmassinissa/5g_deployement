86949fb61ff57d4fefdfa09f5b3bd6b153a04540
Masnssen <masnssen@gmail.com>
2024-04-02 14:34:57 +0200

    new file:   k8s_deployment/inventory/hosts 	new file:   k8s_deployment/k8s_master.yml 	new file:   k8s_deployment/output.sh 	new file:   k8s_deployment/roles/docker-cri/files/cri-dockerd 	new file:   k8s_deployment/roles/docker-cri/files/cri-dockerd.tar.gz 	new file:   k8s_deployment/roles/docker-cri/files/go1.22.1.linux-amd64.tar.gz 	new file:   k8s_deployment/roles/docker-cri/tasks/main.yml 	new file:   k8s_deployment/roles/docker/.travis.yml 	new file:   k8s_deployment/roles/docker/README.md 	new file:   k8s_deployment/roles/docker/tasks/.main.yml.swo 	new file:   k8s_deployment/roles/docker/tasks/.main.yml.swp 	new file:   k8s_deployment/roles/docker/tasks/main.yml 	new file:   k8s_deployment/roles/docker/tests/inventory 	new file:   k8s_deployment/roles/docker/tests/test.yml 	new file:   k8s_deployment/roles/k8s/tasks/main.yml 	new file:   k8s_deployment/roles/k8s_master/files/calico.yml 	new file:   k8s_deployment/roles/k8s_master/files/flannel.yml 	new file:   k8s_deployment/roles/k8s_master/tasks/.main.yml.swp 	new file:   k8s_deployment/roles/k8s_master/tasks/k8s_calico.yml 	new file:   k8s_deployment/roles/k8s_master/tasks/k8s_flannel.yml 	new file:   k8s_deployment/roles/k8s_master/tasks/k8s_info.yml 	new file:   k8s_deployment/roles/k8s_master/tasks/k8s_init.yml 	new file:   k8s_deployment/roles/k8s_master/tasks/main.yml 	new file:   k8s_deployment/roles/k8s_workers/tasks/main.yml

---
+++ k8s_deployment/output.sh
@@ -0,0 +1,1 @@
+kubeadm join 192.168.56.102:6443 --token y7nqqj.m7vd6cwcpmmyhn9k --discovery-token-ca-cert-hash sha256:53257c02d812e5ac85bff01b2373cd54c5404bcd51a33e0d5274439fe48e88f2  --cri-socket=unix:///var/run/cri-dockerd.sock
