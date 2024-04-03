fca45513c9b38afc78ffa39a0bf7e73a96da924c
Masnssen <masnssen@gmail.com>
2024-04-03 17:16:37 +0200

    deleted:    k8s_deployment/inventory/hosts 	deleted:    k8s_deployment/k8s_master.yml 	deleted:    k8s_deployment/output.sh 	deleted:    k8s_deployment/roles/docker-cri/files/cri-dockerd 	deleted:    k8s_deployment/roles/docker-cri/files/cri-dockerd.tar.gz 	deleted:    k8s_deployment/roles/docker-cri/files/go1.22.1.linux-amd64.tar.gz 	deleted:    k8s_deployment/roles/docker-cri/tasks/main.yml 	deleted:    k8s_deployment/roles/docker/.travis.yml 	deleted:    k8s_deployment/roles/docker/README.md 	deleted:    k8s_deployment/roles/docker/tasks/.main.yml.swo 	deleted:    k8s_deployment/roles/docker/tasks/.main.yml.swp 	deleted:    k8s_deployment/roles/docker/tasks/main.yml 	deleted:    k8s_deployment/roles/docker/tests/inventory 	deleted:    k8s_deployment/roles/docker/tests/test.yml 	deleted:    k8s_deployment/roles/k8s/tasks/main.yml 	deleted:    k8s_deployment/roles/k8s_master/files/calico.yml 	deleted:    k8s_deployment/roles/k8s_master/files/flannel.yml 	deleted:    k8s_deployment/roles/k8s_master/tasks/.main.yml.swp 	deleted:    k8s_deployment/roles/k8s_master/tasks/k8s_calico.yml 	deleted:    k8s_deployment/roles/k8s_master/tasks/k8s_flannel.yml 	deleted:    k8s_deployment/roles/k8s_master/tasks/k8s_info.yml 	deleted:    k8s_deployment/roles/k8s_master/tasks/k8s_init.yml 	deleted:    k8s_deployment/roles/k8s_master/tasks/main.yml 	deleted:    k8s_deployment/roles/k8s_workers/tasks/main.yml 	modified:   output.sh 	modified:   roles/docker-cri/tasks/main.yml

--- k8s_deployment/output.sh
+++
@@ -1,1 +0,0 @@
-kubeadm join 192.168.56.101:6443 --token pw00ji.pvjzoz8ln8h8xz6b --discovery-token-ca-cert-hash sha256:1216324ec72382cc2ac7da77ea5d5bdca33cc05068ae8484c1886278b8eab90b  --cri-socket=unix:///var/run/cri-dockerd.sock
--- output.sh
+++ output.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.56.102:6443 --token y7nqqj.m7vd6cwcpmmyhn9k --discovery-token-ca-cert-hash sha256:53257c02d812e5ac85bff01b2373cd54c5404bcd51a33e0d5274439fe48e88f2  --cri-socket=unix:///var/run/cri-dockerd.sock
+kubeadm join 192.168.56.101:6443 --token gn437g.uu1vkratq6vlwovi --discovery-token-ca-cert-hash sha256:f38abf22554248cf5069e538ed156384e23ee4dcbf3b346ff4ea7384a9f104bf  --cri-socket=unix:///var/run/cri-dockerd.sock
