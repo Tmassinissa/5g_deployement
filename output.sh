2e2d7548a693df4e29c967b2a5301d023b9119aa
Masnssen <massinissa.tighilt.lip6.fr>
2024-04-17 14:22:24 +0200

    17042024_1421

--- output.sh
+++ output.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-kubeadm join 100.100.100.100:6443 --token wap5rn.hfxdaiu31lxjga93 --discovery-token-ca-cert-hash sha256:1e34216ed7b53802deb704f62630d071ef0e0d432195684d6f92084a68aa0ebe  --cri-socket=unix:///var/run/cri-dockerd.sock
-=======
-kubeadm join 192.168.56.105:6443 --token m1owo8.v3bihk6hayyzliip --discovery-token-ca-cert-hash sha256:dd0d40ffb9d585b546063e988decee35c853593114645d0e64de264896ecbce4  --cri-socket=unix:///var/run/cri-dockerd.sock
->>>>>>> 2b948005c020469da6dc6283b70beecc42e35ca9
+kubeadm join 192.168.56.101:6443 --token y6hbrt.z3dh5fgu11imgvl4 --discovery-token-ca-cert-hash sha256:86a53b99520aa8a337dd90ffa4403d86e37a350f1f530361f488499408bb6038  --cri-socket=unix:///var/run/cri-dockerd.sock
