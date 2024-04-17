2b948005c020469da6dc6283b70beecc42e35ca9
vagrant <vagrant@kubmaster1>
2024-04-17 09:55:10 +0000

    17/04/2024

---
+++ output.sh
@@ -0,0 +1,1 @@
+kubeadm join 192.168.56.105:6443 --token m1owo8.v3bihk6hayyzliip --discovery-token-ca-cert-hash sha256:dd0d40ffb9d585b546063e988decee35c853593114645d0e64de264896ecbce4  --cri-socket=unix:///var/run/cri-dockerd.sock
